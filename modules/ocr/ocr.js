e9b936587024b4f4a89d706d1d1c24956b02895b
Kerim Güney <kerim.gueney@taskforce-it.de>
2018-01-16 12:52:36 +0100

    make OCR html output work

--- modules/ocr/ocr.js
+++ modules/ocr/ocr.js
@@ -8,21 +8,19 @@ function ocr() {
     while(true) {
         uploaded = "";
         display.screen1.execute();
-        var resultHtml = '';
         if(uploaded === "001") {
             var imagePath = path.join(targetdir.trim(), "ocr.jpg");
             var tesseractPromise = tesseract.create(
                 {
-                    // workerPath: "C:\\Users\\Kerim\\code\\pjs\\node_modules\\tesseract.js\\src\\node\\worker.js",
+                    workerPath: "C:\\Users\\Kerim\\code\\pjs\\node_modules\\tesseract.js\\src\\node\\worker.js",
                     langPath: "eng.traineddata",
-                    // corePath: "C:\\Users\\Kerim\\code\\pjs\\node_modules\\tesseract.js\\src\\node\\index.js"
+                    corePath: "C:\\Users\\Kerim\\code\\pjs\\node_modules\\tesseractjs-core\\index.js"
+                    
                 }
             ).recognize(imagePath, {lang: 'eng'});
             var result = pjs.fiber.runPromise(tesseractPromise);
-            resultHtml = result.html;        
+            html = result.html;        
         }
-        html += resultHtml;
-        console.log('Result: ', resultHtml);
     }
 }
 
