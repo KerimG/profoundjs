6c3ed86275a137d5e9282b9373512e8852264322
Kerim Güney <kerim.gueney@taskforce-it.de>
2018-01-24 13:31:28 +0100

    add route to custom upload

--- config.js
+++ config.js
@@ -5,14 +5,16 @@ module.exports = {
   "pathlist": [
     "pjssamples",
     "ocr",
-    "kerim"
+    "kerim",
+    "test_vog"
   ],
   "initialModules": {
     "/hello": "pjssamples/hello",
     "/hello2": "pjssamples/hello2",
     "/connect4": "pjssamples/connect4",
     "/ocr": "ocr/ocr",
-    "/test_connection": "kerim/test_connection"
+    "/test_connection": "kerim/test_connection",
+    "/vog" : "test_vog/vog"
   },
   "dbDriver": "IBMi",
   "timeout": 3600
